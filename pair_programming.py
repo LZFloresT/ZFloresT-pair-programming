58cc4d80abeaaa312c3bdc5564b2fd654d686446
Luciano Z. Flores <luciano_zurbiaflorestroccoli@mines.edu>
2026-03-30 16:25:22 -0600

    Added text file with function to convert feet/inches to meters ready for Wyatt to check

---
+++ pair_programming.py
@@ -0,0 +1,6 @@
+# Pair Programming Exercise
+# Luciano Z. Flores
+def feet_inches_to_meters(feet, inches):
+    total_inches = (feet * 12) + inches
+    meters = total_inches * 0.0254
+    return meters
