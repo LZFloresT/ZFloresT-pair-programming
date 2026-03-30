619da631d8dcf166490a37b56d32bc62c562b7c6
Wyatt Conger <wyatt_conger@mines.edu>
2026-03-30 16:50:52 -0600

    Added feedback and a couple test functions

--- pair_programming.py
+++ pair_programming.py
@@ -4,3 +4,6 @@ def feet_inches_to_meters(feet, inches):
     total_inches = (feet * 12) + inches
     meters = total_inches * 0.0254
     return meters
+
+# Function looks good, I like the use of using feet and inches as parameters. - Wyatt
+# Also like the clear variable names and function name. - Wyatt
